895169215e7770d1e0fc19b5e803f265d0812e94
chivoi <a.lastovirya@gmail.com>
2021-05-09 16:17:36 +1000

    creates and tests TicketViewer class, it's initialize method and all_tickets method

---
+++ lib/ticket_viewer.rb
@@ -0,0 +1,18 @@
+require 'httparty'
+
+class TicketViewer
+
+  attr_reader :username, :password, :subdomain
+
+  def initialize(username, password, subdomain)
+    @username = username
+    @password = password
+    @subdomain = subdomain
+  end
+
+  def all_tickets()
+    auth = {username: @username, password: @password}
+    tickets = HTTParty.get("https://#{@subdomain}.zendesk.com/api/v2/tickets.json", basic_auth: auth).parsed_response["tickets"]
+  end
+
+end
---
+++ spec/ticket_viewer_spec.rb
@@ -0,0 +1,25 @@
+require_relative "../lib/ticket_viewer"
+
+describe 'TicketViewer' do
+  before(:each) do
+    @ticket_viewer = TicketViewer.new("test@email.com", "1234", "company567")
+  end
+
+  it "should be an instance of TicketViewer" do
+    expect(@ticket_viewer).to be_a TicketViewer
+  end
+  it "should initialize with username and password" do
+    expect(@ticket_viewer.username).to eq("test@email.com")
+    expect(@ticket_viewer.password).to eq("1234")
+    expect(@ticket_viewer.subdomain).to eq("company567")
+  end
+
+  describe('all_tickets') do
+    it 'should return nil if authentication failed' do
+      expect(@ticket_viewer.all_tickets).to be(nil)
+    end
+  end
+
+  # TODO: Testing actual HTTP responses
+
+end
