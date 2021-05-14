a05c45a25881b8a57eb9de5519de9dd75bb087de
chivoi <a.lastovirya@gmail.com>
2021-05-14 18:45:21 +1000

    fixes up existing tests with new method names

--- spec/ticket_viewer_spec.rb
+++ spec/ticket_viewer_spec.rb
@@ -1,43 +1,39 @@
 require_relative "../lib/ticket_viewer"
-require 'json'
+require 'dotenv'
+
+Dotenv.load('.env')
 
 describe 'TicketViewer' do
   before(:each) do
-    @ticket_viewer = TicketViewer.new("test@email.com", "1234", "company567")
-    @response = File.read(File.expand_path("../../test-docs/tickets.json", __FILE__))
-    @data = JSON.parse(@response)["tickets"]
+    @session = TicketViewer.new(ENV["EMAIL"], ENV["SUBDOMAIN"], ENV["PASSWORD"])
+    @data = @session.get_tickets()
   end
 
   it "should be an instance of TicketViewer" do
-    expect(@ticket_viewer).to be_a TicketViewer
+    expect(@session).to be_a TicketViewer
   end
   it "should initialize with username and password" do
-    expect(@ticket_viewer.username).to eq("test@email.com")
-    expect(@ticket_viewer.password).to eq("1234")
-    expect(@ticket_viewer.subdomain).to eq("company567")
+    expect(@session.username).to eq("a.lastovirya@gmail.com")
+    expect(@session.subdomain).to eq("ana4256")
   end
 
-  describe('all_tickets') do
-    it 'should get an error when authentication fails' do
-      expect(@ticket_viewer.get_tickets).to include("error")
+  describe('get_tickets') do
+    it 'should get a hash with an array of tickets that is not empty' do
+      expect(@session.get_tickets()).to include("tickets")
     end
   end
-
-  # TODO: Testing actual HTTP responses
 
   describe('single_ticket') do
     it 'should return the correct ticket' do
       id = 1
-      ticket = @ticket_viewer.single_ticket(@data, id)
-      expect(ticket[0]["assignee_id"]).to eq(5)
+      ticket = @session.get_single_ticket(@data["tickets"], id)
+      expect(ticket["id"]).to eq(1)
     end
   end
 
   describe('display_page') do
     it 'should return a table' do
-      expect(@ticket_viewer.display_page(@data)).to be_a Terminal::Table
+      expect(@session.display_data(@data)).to be_a Terminal::Table
     end
   end
-
-
 end
